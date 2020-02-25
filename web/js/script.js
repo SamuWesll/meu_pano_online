f79cf5ae4be727e63dc350665bcd9da344bed32e
Karina Aquino <karinaapthomaz@gmail.com>
2020-02-25 19:44:58 -0300

    Página Estatica Produto Detalhado

---
+++ web/js/script.js
@@ -0,0 +1,40 @@
+let inputCEP = document.getElementById('cepDetalhe');
+let btnPesquisarCEP = document.getElementById('btnPesquisarCEP');
+
+let config = {
+    method: "GET"
+}
+
+/*function buscarCep(cep){
+    alert('Buscando CEP');
+    let config = {
+        method:"get"
+    }
+    fetch(`http://viacep.com.br/ws/${cep}/json/`, config)
+    .then(response => response.json())
+    .then(dados =>{
+        if(dados.erro){ 
+            return inputCEP.setAttribute('class', 'form-control is-invalid')
+        }
+        inputCEP.setAttribute('class', 'form-control is-valid')
+        
+        
+    })
+}*/
+
+inputCEP.addEventListener('keyup',  (event)=>{
+        if(isNaN(inputCEP.value)){
+            inputCEP.value = inputCEP.value.substring(0, (inputCEP.value.length -1))
+        }
+        if(inputCEP.value.length > 8){
+            inputCEP.value = inputCEP.value.substring(0, 8)
+        }
+})
+
+btnPesquisarCEP.addEventListener('click', (event)=>{
+    if(inputCEP.value.length<8){
+        alert('Insira um CEP');
+    }else{
+        alert('Buscando CEP');
+    }
+})
