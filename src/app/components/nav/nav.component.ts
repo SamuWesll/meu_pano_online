bd0e82c3a4ec3e6adb097d873b7c8f14bcd23004
Karina Thomaz <karinaapthomaz@gmail.com>
2020-03-12 01:05:34 -0300

    Nav Hamburger

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+interface iCategoria{
+  label: string;
+  route: string;
+  ativo: boolean;
+}
+
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
@@ -7,7 +13,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavComponent implements OnInit {
 
+    item:iCategoria[]=[
+    {
+      ativo: true,
+      label: "Todas as Categorias",
+      route: "/todas"
+    },
+    {
+      ativo: false,
+      label: "Pop",
+      route: "/pop"
+    },
+    {
+      ativo: false,
+      label: "Bordados",
+      route: "/bordados"
+    },
+    {
+      ativo: false,
+      label: "Pintados",
+      route: "/pintados"
+    },
+    {
+      ativo: false,
+      label: "Frases",
+      route: "/frases"
+    }
+  ]
+
   constructor() { }
+
+  destacar(item: iCategoria){
+    this.item.forEach(
+      (item)=>{item.ativo=false}
+    )
+    item.ativo=true;
+  }
 
   ngOnInit(): void {
   }
