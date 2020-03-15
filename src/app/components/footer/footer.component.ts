722de5aabd5f83021bac60872a1814e37e563351
ronaldomendes <mendes-ronaldo@live.com>
2020-03-15 20:28:45 -0300

    Editando o componente produtos

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  ano: number = new Date().getFullYear();
   constructor() { }
 
   ngOnInit(): void {
