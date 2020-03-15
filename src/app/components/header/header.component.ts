a2e1ffac75434673ed18398a1b8782ec46942bac
Samuel Weslley M. Barboza <SWBarboza@raiadrogasil.com>
2020-03-15 10:45:33 -0300

    implementação do login

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
 import { LoginComponent } from '../login/login.component';
+import { Cliente } from 'src/app/models/Cliente';
 
 @Component({
   selector: 'app-header',
@@ -10,11 +11,31 @@ import { LoginComponent } from '../login/login.component';
 export class HeaderComponent implements OnInit {
 
   modalRef: BsModalRef
+  public login: Cliente;
 
   openModal() {
     this.modalRef = this.modalService.show(LoginComponent)
   }
-  constructor(private modalService: BsModalService) { }
+  constructor(private modalService: BsModalService) {
+
+    this.verificarLogin();
+
+    // this.sairLogin();
+
+  }
+
+  verificarLogin() {
+
+    this.login = JSON.parse(localStorage.getItem("logado"));
+    console.log(this.login);
+
+  }
+
+  sairLogin() {
+
+    localStorage.removeItem("logado");
+
+  }
 
   ngOnInit(): void {
   }
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/models/Cliente';
+import { Sexo } from 'src/app/models/Sexo';
+import { DatePipe, formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +10,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public logado: Cliente;
+
+  constructor() {
+
+    // this.logadoLocalStorage();
+
+    // this.sairLogin();
+    // console.log(this.logado)
+
+  }
+
+  logadoLocalStorage() {
+
+    let cliente = {
+      idCliente: 1,
+      nome: "Samuel Weslley Rocha Barboza",
+      cpf: "41851766865",
+      email: "samuelwrochabarboza@gmail.com",
+      senha: "samuka",
+      dataNascimento: "08/12/1994",
+      genero: Sexo.masculino,
+    }
+
+    localStorage.setItem("logado", JSON.stringify(cliente));
+
+    this.logado = new Cliente(
+      cliente.idCliente,
+      cliente.nome,
+      cliente.cpf,
+      cliente.email,
+      cliente.senha,
+      cliente.dataNascimento,
+      cliente.genero
+    )
+  }
+
+  sairLogin() {
+
+    localStorage.removeItem("logado");
+
+  }
 
   ngOnInit(): void {
   }
