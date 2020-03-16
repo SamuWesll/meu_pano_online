b1e11ec3f465c41fad82914c31ba21843ea55cf6
Treinamento <Treinamento@RD723903.raiadrogasil.com>
2020-03-16 10:41:24 -0300

    Editando a lista de produtos

--- src/app/components/lista-de-produtos/lista-de-produtos.component.ts
+++ src/app/components/lista-de-produtos/lista-de-produtos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Produtos } from 'src/app/models/Produtos';
+import { HttpService } from 'src/app/services/http.service';
 
 @Component({
   selector: 'app-lista-de-produtos',
@@ -10,8 +11,16 @@ export class ListaDeProdutosComponent implements OnInit {
 
   public produtos: Produtos[] = [];
 
+  converteDecimal(valor: string): string {
+    return parseFloat(valor).toFixed(2).replace('.', ',')
+  };
 
-  constructor() {}
+  tresParcelas(valor: number): string {
+    valor /= 3
+    return valor.toFixed(2).replace('.', ',')
+  }
+
+  constructor(public http: HttpService) {}
 
   ngOnInit(): void {
   }
