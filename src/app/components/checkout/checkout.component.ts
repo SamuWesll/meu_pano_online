a808e486f9d82c3a380e20dae14ed0444490523d
Treinamento <Treinamento@RD723932.raiadrogasil.com>
2020-03-19 16:53:56 -0300

    validaçoes

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,7 +1,7 @@
 import { HttpClientModule } from '@angular/common/http';
 import { HttpService } from './../../services/http.service';
 import { Component, OnInit, Input } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, AbstractControl } from '@angular/forms';
 import { Checkout } from 'src/app/models/Checkout';
 import { HttpClient } from "@angular/common/http";
 // import 'rxjs/add/operator/map';
@@ -73,6 +73,56 @@ export class CheckoutComponent implements OnInit {
   }
 
   constructor(private Http: HttpClient) { }
+
+  static isValidCpf() {
+    return (control: AbstractControl): Validators => {
+      const cpf = control.value;
+      if (cpf) {
+        let numbers, digits, sum, i, result, equalDigits;
+        equalDigits = 1;
+        if (cpf.length < 11) {
+         return null;
+        }
+
+        for (i = 0; i < cpf.length - 1; i++) {
+          if (cpf.charAt(i) !== cpf.charAt(i + 1)) {
+            equalDigits = 0;
+            break;
+          }
+        }
+
+        if (!equalDigits) {
+          numbers = cpf.substring(0, 9);
+          digits = cpf.substring(9);
+          sum = 0;
+          for (i = 10; i > 1; i--) {
+            sum += numbers.charAt(10 - i) * i;
+          }
+
+          result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+          if (result !== Number(digits.charAt(0))) {
+            return { cpfNotValid: true };
+          }
+          numbers = cpf.substring(0, 10);
+          sum = 0;
+
+          for (i = 11; i > 1; i--) {
+            sum += numbers.charAt(11 - i) * i;
+          }
+          result = sum % 11 < 2 ? 0 : 11 - (sum % 11);
+
+          if (result !== Number(digits.charAt(1))) {
+            return { cpfNotValid: true };
+          }
+          return null;
+        } else {
+          return { cpfNotValid: true };
+        }
+     }
+   return null;
+ };
+}
 
   ngOnInit(): void {
   }
