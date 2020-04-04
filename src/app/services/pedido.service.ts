908f6917525d14e259869ea27f7f2a27e2de3ccc
Karina Aparecida Thomaz de Aquino <KAAquino@raiadrogasil.com>
2020-04-04 14:23:07 -0300

    atualizando service pedido

--- src/app/services/pedido.service.ts
+++ src/app/services/pedido.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Pedido } from "../models/Pedido";
 
 const URLPedido: string = "http://localhost:8080/meupanoonline/pedido" ;
 
@@ -17,4 +20,15 @@ export class PedidoService{
         return this.pedidoService.post(URLPedido, body);
     }
 
+    exibir(idPedido): Observable<Pedido>{
+        return this.pedidoService.get<Pedido>(`$URLPedido/${idPedido}`).pipe(
+            catchError(_ => of(null))
+        );
+    }
+
+    cancelar(idPedido): Observable<Pedido>{
+        return this.pedidoService.patch<Pedido>(`{URLPedido}/deletar${idPedido}`, null).pipe(
+            catchError(_ => of(null))
+        )
+    }
 }
