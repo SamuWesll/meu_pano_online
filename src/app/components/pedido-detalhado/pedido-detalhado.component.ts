940f516a6678edea3446a69c4e199a83a66b84d7
Karina Thomaz <karinaapthomaz@gmail.com>
2020-04-13 09:57:17 -0300

    ts pedido detalhado

---
+++ src/app/components/pedido-detalhado/pedido-detalhado.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from "@angular/core";
+import { PedidoService } from 'src/app/services/pedido.service';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Pedido } from 'src/app/models/Pedido';
+
+export class PedidoDetalhadoComponent implements OnInit{
+    
+    constructor(private pedidoService: PedidoService, private route: ActivatedRoute){}
+
+    pedido: Observable<Pedido>;
+
+    ngOnInit(){
+        this.pedido = this.pedidoService.exibir(this.route.snapshot.paramMap.get('id'));
+    }
+    
+}
